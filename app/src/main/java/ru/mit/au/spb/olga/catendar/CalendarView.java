2a054e596b3045aa8a1da010bf3f85fd4ea8043c
LizaTretyakova <elizabet.tretyakova@gmail.com>
2015-10-26 23:09:55 +0300

    xml and java-class

---
+++ app/src/main/java/ru/mit/au/spb/olga/catendar/CalendarView.java
@@ -0,0 +1,12 @@
+package ru.mit.au.spb.olga.catendar;
+
+import android.content.Context;
+import android.view.View;
+
+public class CalendarView extends View {
+    public CalendarView(Context context) {
+        super(context);
+    }
+
+
+}
