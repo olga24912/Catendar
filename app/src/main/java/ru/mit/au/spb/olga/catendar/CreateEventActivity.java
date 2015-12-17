aaacec887af4b79cc398e8efb73b5b742172db5b
Olga Chernikova <Oche24912@yandex.ru>
2015-12-17 10:44:09 +0300

    beautiful create event

--- app/src/main/java/ru/mit/au/spb/olga/catendar/CreateEventActivity.java
+++ app/src/main/java/ru/mit/au/spb/olga/catendar/CreateEventActivity.java
@@ -12,6 +12,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.TimePicker;
 
@@ -21,25 +22,30 @@ import java.util.GregorianCalendar;
 /**
  * Created by olga on 18.10.15.
  */
-public class CreateEventActivity extends AppCompatActivity {
+public class CreateEventActivity extends AppCompatActivity
+        implements SeekBar.OnSeekBarChangeListener {
     private DatabaseHelper mDatabaseHelper;
     private SQLiteDatabase mSQLiteDatabase;
 
     private EditText eventText;
 
-    int DIALOG_DATE = 1;
+    private int DIALOG_DATE = 1;
     private DatePicker pickerDate;
 
-    int DIALOG_TIME = 2;
-    int hour = 14;
-    int minute = 00;
+    private int DIALOG_TIME = 2;
+    private int hour = 14;
+    private int minute = 00;
 
-    TextView tvInfo;
-    TextView tvInfoStartTime;
+    private TextView tvInfo;
+    private TextView tvInfoStartTime;
+
+    private TextView lenOfEvent;
 
     private int year;
     private int month;
     private int day;
+
+    private int duration;
 
     public final static String EVENT_NAME = "ru.mit.au.spb.olga.catendar.eventName";
 
@@ -56,6 +62,15 @@ public class CreateEventActivity extends AppCompatActivity {
 
         tvInfo = (TextView)findViewById(R.id.StartDate);
         tvInfoStartTime = (TextView)findViewById(R.id.StartTime);
+        lenOfEvent = (TextView)findViewById(R.id.durationVal);
+        lenOfEvent.setText("1");
+
+        final SeekBar seekbar = (SeekBar)findViewById(R.id.seekBarOfEventLen);
+        seekbar.setOnSeekBarChangeListener(this);
+        seekbar.setMax(24);
+        seekbar.setProgress(1);
+
+        duration = 1;
 
         mDatabaseHelper = new DatabaseHelper(this, "mydatabase10.db", null, 1);
         mSQLiteDatabase = mDatabaseHelper.getWritableDatabase();
@@ -64,6 +79,8 @@ public class CreateEventActivity extends AppCompatActivity {
         year = today.get(Calendar.YEAR);
         month = today.get(Calendar.MONTH);
         day = today.get(Calendar.DAY_OF_MONTH);
+        tvInfoStartTime.setText("Start time is " + hour + " hours " + minute + " minutes");
+        tvInfo.setText("Event day is " + day + "/" + (month + 1) + "/" + year);
     }
 
     public void onSetDateClick(View view) {
@@ -93,14 +110,14 @@ public class CreateEventActivity extends AppCompatActivity {
             year = _year;
             month = monthOfYear;
             day = dayOfMonth;
-            tvInfo.setText("Event day is " + day + "/" + month + "/" + year);
+            tvInfo.setText("Event day is " + day + "/" + (month + 1) + "/" + year);
         }
     };
 
     TimePickerDialog.OnTimeSetListener myCallBackTime = new TimePickerDialog.OnTimeSetListener() {
-        public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+        public void onTimeSet(TimePicker view, int hourOfDay, int _minute) {
             hour = hourOfDay;
-            minute = minute;
+            minute = _minute;
             tvInfoStartTime.setText("Start time is " + hour + " hours " + minute + " minutes");
         }
     };
@@ -116,11 +133,11 @@ public class CreateEventActivity extends AppCompatActivity {
         GregorianCalendar startCal = new GregorianCalendar(year, month, day, hour, minute);
 
         GregorianCalendar endCal = startCal;
-        endCal.add(Calendar.HOUR_OF_DAY, 1);
 
         newValues.put(DatabaseHelper.EVENT_NAME, String.valueOf(createEvent.getText()));
         newValues.put(DatabaseHelper.EVENT_PARENT_TEMPLATE, 0);
         newValues.put(DatabaseHelper.EVENT_START_DATE, startCal.getTimeInMillis()/1000);
+        endCal.add(Calendar.HOUR_OF_DAY, duration);
         newValues.put(DatabaseHelper.EVENT_END_DATE, endCal.getTimeInMillis()/1000);
 
         mSQLiteDatabase.insert("events", null, newValues);
@@ -134,5 +151,21 @@ public class CreateEventActivity extends AppCompatActivity {
     public void onCancelClick(View view) {
         setResult(RESULT_CANCELED);
         finish();
+    }
+
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+
+    }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+
+    }
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
+        lenOfEvent.setText(String.valueOf(seekBar.getProgress()));
+        duration = seekBar.getProgress();
     }
 }
