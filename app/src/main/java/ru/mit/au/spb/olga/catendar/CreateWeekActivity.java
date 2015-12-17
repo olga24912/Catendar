2b5c6291a35cf49a818fd826964bc4e63590f1db
Olga Chernikova <Oche24912@yandex.ru>
2015-12-17 16:02:40 +0300

    change create week

--- app/src/main/java/ru/mit/au/spb/olga/catendar/CreateWeekActivity.java
+++ app/src/main/java/ru/mit/au/spb/olga/catendar/CreateWeekActivity.java
@@ -1,5 +1,8 @@
 package ru.mit.au.spb.olga.catendar;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.TimePickerDialog;
 import android.content.ContentValues;
 import android.content.Intent;
 import android.database.Cursor;
@@ -9,11 +12,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.CheckBox;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.RadioGroup;
+import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.GregorianCalendar;
 
 /**
@@ -23,11 +29,18 @@ public class CreateWeekActivity extends AppCompatActivity {
     private DatabaseHelper mDatabaseHelper;
     private SQLiteDatabase mSQLiteDatabase;
 
-    private EditText weekDate;
     private ArrayList<CheckBox> existsTemplate = new ArrayList<>();
     private ArrayList<Integer> templateId = new ArrayList<>();
 
     private int selectedRadioButton = -1;
+
+    private int DIALOG_DATE = 1;
+
+    private int year;
+    private int month;
+    private int day;
+
+    private TextView tvInfo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,7 +51,7 @@ public class CreateWeekActivity extends AppCompatActivity {
         mDatabaseHelper = new DatabaseHelper(this, "mydatabase10.db", null, 1);
         mSQLiteDatabase = mDatabaseHelper.getWritableDatabase();
 
-        weekDate = (EditText)findViewById(R.id.write_date);
+        tvInfo = (TextView)findViewById(R.id.Selected_date);
 
         createCheckBox();
 
@@ -47,16 +60,39 @@ public class CreateWeekActivity extends AppCompatActivity {
         radiogroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(RadioGroup group, int checkedId) {
+                Calendar today;
+                Week week;
                 switch (checkedId) {
                     case -1:
                         break;
                     case R.id.current_week:
+                        today = Calendar.getInstance();
+                        year = today.get(Calendar.YEAR);
+                        month = today.get(Calendar.MONTH);
+                        day = today.get(Calendar.DAY_OF_MONTH);
+                        week = new Week(new GregorianCalendar(year, month, day));
+                        year = week.getStartDate().get(Calendar.YEAR);
+                        month = week.getStartDate().get(Calendar.MONTH);
+                        day = week.getStartDate().get(Calendar.DAY_OF_MONTH);
                         selectedRadioButton = 0;
+                        tvInfo.setText("");
                         break;
                     case R.id.next_week:
+                        today = Calendar.getInstance();
+                        year = today.get(Calendar.YEAR);
+                        month = today.get(Calendar.MONTH);
+                        day = today.get(Calendar.DAY_OF_MONTH);
+                        week = new Week(new GregorianCalendar(year, month, day));
+                        GregorianCalendar nextWeek = week.getStartDate();
+                        nextWeek.add(Calendar.DAY_OF_MONTH, 7);
+                        year = week.getStartDate().get(Calendar.YEAR);
+                        month = week.getStartDate().get(Calendar.MONTH);
+                        day = week.getStartDate().get(Calendar.DAY_OF_MONTH);
                         selectedRadioButton = 1;
+                        tvInfo.setText("");
                         break;
                     case R.id.other_week:
+                        showDialog(DIALOG_DATE);
                         selectedRadioButton = 2;
                         break;
 
@@ -65,6 +101,10 @@ public class CreateWeekActivity extends AppCompatActivity {
                 }
             }
         });
+        Calendar today = Calendar.getInstance();
+        year = today.get(Calendar.YEAR);
+        month = today.get(Calendar.MONTH);
+        day = today.get(Calendar.DAY_OF_MONTH);
     }
 
     private void createCheckBox() {
@@ -98,6 +138,27 @@ public class CreateWeekActivity extends AppCompatActivity {
         }
     }
 
+    protected Dialog onCreateDialog(int id) {
+        if (id == DIALOG_DATE) {
+            DatePickerDialog tpd = new DatePickerDialog(this, (DatePickerDialog.OnDateSetListener) myCallBackDate, year, month, day);
+            return tpd;
+        }
+        return super.onCreateDialog(id);
+    }
+
+
+    DatePickerDialog.OnDateSetListener myCallBackDate = new DatePickerDialog.OnDateSetListener() {
+
+        public void onDateSet(DatePicker view, int _year, int monthOfYear,
+                              int dayOfMonth) {
+            Week week = new Week(new GregorianCalendar(_year, monthOfYear, dayOfMonth));
+            year = week.getStartDate().get(Calendar.YEAR);
+            month = week.getStartDate().get(Calendar.MONTH);
+            day = week.getStartDate().get(Calendar.DAY_OF_MONTH);
+            tvInfo.setText("Event day is " + day + "/" + (month + 1) + "/" + year);
+        }
+    };
+
     private Integer findIdWithThisTime(long ms) {
         Cursor cursor = mSQLiteDatabase.query(DatabaseHelper.DATABASE_TABLE_WEEK, new String[]{
                         DatabaseHelper._ID, DatabaseHelper.WEEK_START_DATE
@@ -118,10 +179,7 @@ public class CreateWeekActivity extends AppCompatActivity {
     }
 
     public void onOkWeekClick(View view) {
-        String date = String.valueOf(weekDate.getText());
-
-        GregorianCalendar gc = new GregorianCalendar(yearFromDate(date), monthFromDate(date),
-                dayFromDate(date));
+        GregorianCalendar gc = new GregorianCalendar(year, month, day);
 
         Week nw = new Week(gc);
         long sTime = nw.getTimeInMS();
@@ -155,65 +213,4 @@ public class CreateWeekActivity extends AppCompatActivity {
         setResult(RESULT_CANCELED);
         finish();
     }
-
-    private int yearFromDate (String date) {
-        String[] ddmmyyyy = date.split("\\.");
-        if (ddmmyyyy.length < 3 || notCorrectYear(ddmmyyyy[2])) {
-            return 9999;
-        }
-        return Integer.parseInt(ddmmyyyy[2]);
-    }
-
-    private int monthFromDate (String date) {
-        String[] ddmmyyyy = date.split("\\.");
-        if (ddmmyyyy.length < 2 || notCorrectMonth(ddmmyyyy[1])){
-            return 01;
-        }
-        return Integer.parseInt(ddmmyyyy[1]);
-    }
-
-    private int dayFromDate (String date) {
-        String[] ddmmyyyy = date.split("\\.");
-        if (ddmmyyyy.length < 1 || notCorrectDay(ddmmyyyy[0])) {
-            return 01;
-        }
-        return Integer.parseInt(ddmmyyyy[0]);
-    }
-
-    Boolean notCorrectMonth(String month) {
-        try {
-            int vl = Integer.parseInt(month);
-            if (vl < 1 || vl > 12) {
-                return true;
-            }
-        } catch (Exception ignored) {
-            return true;
-        }
-        return false;
-    }
-
-    Boolean notCorrectYear(String year) {
-        try {
-            int vl = Integer.parseInt(year);
-            if (vl < 0 || vl > 9999) {
-                return true;
-            }
-        } catch (Exception ignored) {
-            return true;
-        }
-        return false;
-    }
-
-    Boolean notCorrectDay(String day) {
-        try {
-            int vl = Integer.parseInt(day);
-            if (vl < 1 || vl > 31) {
-                return true;
-            }
-        } catch (Exception ignored) {
-            return true;
-        }
-        return false;
-    }
-
 }
