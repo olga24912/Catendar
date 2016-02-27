b021efab965ad1a6ea957f97e798bb13016e7b60
Olga Chernikova <Oche24912@yandex.ru>
2016-02-27 13:22:48 +0300

    check task

--- app/src/main/java/ru/mit/au/spb/olga/catendar/Task.java
+++ app/src/main/java/ru/mit/au/spb/olga/catendar/Task.java
@@ -3,9 +3,6 @@ package ru.mit.au.spb.olga.catendar;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 
-/**
- * Created by olga on 18.10.15.
- */
 public class Task {
     private Boolean isDone;
     private String taskText;
