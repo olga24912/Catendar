7ec72fc972d6da82822243914acb2adab21e7b28
LizaTretyakova <elizabet.tretyakova@gmail.com>
2016-03-02 22:02:46 +0300

    Changes in Event and Task because of the CalendarPrimitive introducing

--- app/src/main/java/ru/mit/au/spb/olga/catendar/model/Event.java
+++ app/src/main/java/ru/mit/au/spb/olga/catendar/model/Event.java
@@ -4,64 +4,25 @@ import java.text.DateFormatSymbols;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 
-public class Event {
-    private String eventText;
-    private GregorianCalendar eventStartDate;
-    private GregorianCalendar eventDuration;
-
-    private int id;
+public class Event extends CalendarPrimitive {
 
     private static final String[] days = new DateFormatSymbols().getShortWeekdays();
 
-    public void setId(int dataBaseId) {
-        id = dataBaseId;
-    }
-
-    public int getId() {
-        return id;
-    }
-
     public String getDayOfWeekAndTime() {
-        return "(" + days[eventStartDate.get(Calendar.DAY_OF_WEEK) - 1] + " "
-                + eventStartDate.get(Calendar.HOUR_OF_DAY) + ":"
-                + eventStartDate.get(Calendar.MINUTE) + " - "
+        return "(" + days[startDate.get(Calendar.DAY_OF_WEEK) - 1] + " "
+                + startDate.get(Calendar.HOUR_OF_DAY) + ":"
+                + startDate.get(Calendar.MINUTE) + " - "
                 + getEndDate().get(Calendar.HOUR_OF_DAY) + ":"
                 + getEndDate().get(Calendar.MINUTE) + ")";
     }
 
-    public void setText(String newText) {
-        eventText = newText;
-    }
-
-    public String getText() {
-        return eventText;
-    }
-
-    public GregorianCalendar getStartDate() {
-        return eventStartDate;
-    }
-
     public GregorianCalendar getEndDate() {
         GregorianCalendar endDate = new GregorianCalendar();
-        endDate.setTimeInMillis(eventStartDate.getTimeInMillis() + eventDuration.getTimeInMillis());
+        endDate.setTimeInMillis(startDate.getTimeInMillis() + duration.getTimeInMillis());
         return endDate;
     }
 
-    public long getStart() {
-        return eventStartDate.getTimeInMillis() / 1000;
-    }
-
-    public long getEnd() {
+    public long getEndDateInSeconds() {
         return getEndDate().getTimeInMillis() / 1000;
-    }
-
-    public void setStartDate(int seconds) {
-        eventStartDate = new GregorianCalendar();
-        eventStartDate.setTimeInMillis(seconds * 1000L);
-    }
-
-    public void setEventDuration(int seconds) {
-        eventDuration = new GregorianCalendar();
-        eventDuration.setTimeInMillis(seconds * 1000L);
     }
 }
--- app/src/main/java/ru/mit/au/spb/olga/catendar/model/Task.java
+++ app/src/main/java/ru/mit/au/spb/olga/catendar/model/Task.java
@@ -3,31 +3,10 @@ package ru.mit.au.spb.olga.catendar.model;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 
-public class Task {
+public class Task extends CalendarPrimitive {
     private Boolean isDone;
-    private String taskText;
-    private String comment;
     private int priority;
-    private GregorianCalendar duration;
-    private GregorianCalendar startTime;
     private GregorianCalendar deadlineTime;
-    private Long id;
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Long getId() {
-        return this.id;
-    }
-
-    public String getTaskText() {
-        return taskText;
-    }
-
-    public void changeText(String text) {
-        taskText = text;
-    }
 
     public void changeIsDone(Boolean isDone) {
         this.isDone = isDone;
@@ -35,22 +14,6 @@ public class Task {
 
     public Boolean getIsDone() {
         return isDone;
-    }
-
-    public void setComment(String text) {
-        comment = text;
-    }
-
-    public  String getCommentText() {
-        return comment;
-    }
-
-    public void setStartTime(GregorianCalendar startTime) {
-        this.startTime = startTime;
-    }
-
-    public void setDuration(GregorianCalendar duration) {
-        this.duration = duration;
     }
 
     public String getStringDuration() {
@@ -88,11 +51,4 @@ public class Task {
         return (deadlineTime.getTimeInMillis()/1000);
     }
 
-    public long getDurationTimeInSecond() {
-        return (duration.getTimeInMillis()/1000);
-    }
-
-    public long getStartTimeInSecond() {
-        return (startTime.getTimeInMillis()/1000);
-    }
 }
