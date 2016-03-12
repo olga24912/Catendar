69387a1ce4f257a4bd5b50d717a864350e6b6497
Olga Chernikova <Oche24912@yandex.ru>
2016-03-12 15:17:38 +0300

    enum

--- app/src/main/java/ru/mit/au/spb/olga/catendar/view/calendar/SimpleGestureFilter.java
+++ app/src/main/java/ru/mit/au/spb/olga/catendar/view/calendar/SimpleGestureFilter.java
@@ -4,19 +4,11 @@ import android.view.GestureDetector;
 import android.view.GestureDetector.SimpleOnGestureListener;
 import android.view.MotionEvent;
 
-public class SimpleGestureFilter extends SimpleOnGestureListener{
-
-    public final static int SWIPE_UP    = 1;
-    public final static int SWIPE_DOWN  = 2;
-    public final static int SWIPE_LEFT  = 3;
-    public final static int SWIPE_RIGHT = 4;
-
-    public final static int MODE_SOLID       = 1;
-    public final static int MODE_DYNAMIC     = 2;
+public class SimpleGestureFilter extends SimpleOnGestureListener {
+    public enum Swipe_direction {UP, DOWN, LEFT, RIGHT}
 
     private final static int ACTION_FAKE = -13;
 
-    private int mode             = MODE_DYNAMIC;
     private boolean tapIndicator = false;
 
     private Activity context;
@@ -30,22 +22,15 @@ public class SimpleGestureFilter extends SimpleOnGestureListener{
         this.listener = sgl;
     }
 
-    public void onTouchEvent(MotionEvent event){
+    public void onTouchEvent(MotionEvent event) {
         boolean result = this.detector.onTouchEvent(event);
-
-        if(this.mode == MODE_SOLID)
+        if (event.getAction() == ACTION_FAKE)
+            event.setAction(MotionEvent.ACTION_UP);
+        else if (result)
             event.setAction(MotionEvent.ACTION_CANCEL);
-        else if (this.mode == MODE_DYNAMIC) {
-
-            if(event.getAction() == ACTION_FAKE)
-                event.setAction(MotionEvent.ACTION_UP);
-            else if (result)
-                event.setAction(MotionEvent.ACTION_CANCEL);
-            else if(this.tapIndicator){
-                event.setAction(MotionEvent.ACTION_DOWN);
-                this.tapIndicator = false;
-            }
-
+        else if (this.tapIndicator) {
+            event.setAction(MotionEvent.ACTION_DOWN);
+            this.tapIndicator = false;
         }
     }
 
@@ -68,17 +53,17 @@ public class SimpleGestureFilter extends SimpleOnGestureListener{
         int swipe_Min_Distance = 100;
         if(velocityX > swipe_Min_Velocity && xDistance > swipe_Min_Distance){
             if(e1.getX() > e2.getX())
-                this.listener.onSwipe(SWIPE_LEFT);
+                this.listener.onSwipe(Swipe_direction.LEFT);
             else
-                this.listener.onSwipe(SWIPE_RIGHT);
+                this.listener.onSwipe(Swipe_direction.RIGHT);
 
             result = true;
         }
         else if(velocityY > swipe_Min_Velocity && yDistance > swipe_Min_Distance){
             if(e1.getY() > e2.getY())
-                this.listener.onSwipe(SWIPE_UP);
+                this.listener.onSwipe(Swipe_direction.UP);
             else
-                this.listener.onSwipe(SWIPE_DOWN);
+                this.listener.onSwipe(Swipe_direction.DOWN);
 
             result = true;
         }
@@ -105,18 +90,13 @@ public class SimpleGestureFilter extends SimpleOnGestureListener{
 
     @Override
     public boolean onSingleTapConfirmed(MotionEvent arg) {
-
-        if(this.mode == MODE_DYNAMIC){
             arg.setAction(ACTION_FAKE);
             this.context.dispatchTouchEvent(arg);
-        }
-
         return false;
     }
 
     public interface SimpleGestureListener{
-        void onSwipe(int direction);
+        void onSwipe(Swipe_direction direction);
         void onDoubleTap();
     }
-
 }
